5115c327562a7df0f54e1c4d5d97ef41f4e0dc69
Smartbyme15 <laibakaimkhani2004@gmail.com>
2024-12-07 10:29:57 -0800

    Add files via upload

---
+++ javascript/chapter02/aap.js
@@ -0,0 +1,57 @@
+// question no:01
+// 1. Declare a variable called username.
+var username ;
+// question no:02
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+var myName = "my Full Name";
+// question no:03
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var message="hello world";
+alert(message);
+// question no: 04
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+var Name="laiba";
+var Age=19;
+var percentage="79%";
+var college ="CAcollege";
+alert("Name :" + Name);
+alert("Age :"+ Age);
+alert("persentage :"+  percentage);
+alert("college :"+  college);
+// question no:05
+// 5. Write a script to display the following alert using one JS
+// variable:
+var message="hi thanks for visiting my web \nwhich kind of help do you want? ";
+alert(message);
+// question no:06
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+var email="xyz5@gmail.com";
+var Email="my email is :"+  email ;
+alert(Email);
+// question no:07
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book="A samrter way to learn javascript";
+var line="I am learning javascript from this book :"+ book;
+alert(line);
+//question no:08
+// 8. Write a script to display this in browser through JS
+var text="assalam o alikum <br>";
+document.write(text);
+// question no:09
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+var ms="   i am glad to visting my website ";
+alert(ms);
+document.write(ms);
+
+
